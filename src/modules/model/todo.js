314752556152bad4a850e11d456ef094f5dd5e90
saumya <saumya>
2013-02-01 13:00:30 +0530

    added event handler to model change event

--- src/modules/model/todo.js
+++ src/modules/model/todo.js
@@ -5,10 +5,16 @@ define('modules/model/todo',function(){
 	  // Default todo attribute values
 	  initialize: function(){
 	      console.log('This model has been initialized.');
+	       this.on('change', this.onModelChange);//adding event handlers
 	  },
 	  defaults: {
 	    title: 'DefaultTitle',
 	    completed: false
+	  },
+	  onModelChange : function(ev){
+	  	console.log('on Model Change');
+	  	console.log(ev);
+	  	console.log('---------------');
 	  }
 	});
 	//end backbone
