23887a07b461008b8d66f4a6c403c7dddf17d242
saumya <saumya>
2013-02-01 15:38:41 +0530

    routes is done. Now to view

--- src/modules/appRouter.js
+++ src/modules/appRouter.js
@@ -3,14 +3,20 @@ define('modules/appRouter',function(){
 		//TODO: code the routes
 		/* define the route and function maps for this router */
 	    routes: {
+	    	"home" : "showHome",
 	    	"about" : "showAbout",
 	    	"comingUp" : "showComingUp"
+	    },
+	    showHome: function(){
+	    	console.log('showHome');
 	    },
 		showAbout: function(){
 			console.log('showAbout');
 		},
 		showComingUp: function(){
-			console.log('showComingUp');
+			console.log('showComingUp :');
+			//console.log('showComingUp : Currently nothing so moving to showAbout');
+			//this.navigate('about');
 		}
 	});
 	return applicationRouter;
